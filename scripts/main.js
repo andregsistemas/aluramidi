e7d9d437ce80b6ea517a91bd825bcde276df689a
dukeossuna <andregsistemas@outlook.com>
2023-10-26 13:24:44 -0300

    Alura Midi - Finalizado

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio)
+
+    if(elemento && elemento.localName === 'audio'){
+        elemento.play();
+    }else {
+        alert('Elemento ou seletor inválido!')
+    }
+
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
@@ -25,5 +32,5 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++ ){
     tecla.onkeyup = function() {
         tecla.classList.remove('ativa')
     }
-    
+
 }
